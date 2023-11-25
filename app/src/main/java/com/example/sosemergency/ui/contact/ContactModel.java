5a28242a054397bf8f4926e05bf4966ed44e691d
Othmane berhi <omaxnoti@gmail.com>
2023-11-25 05:56:31 +0000

    created contact model

---
+++ app/src/main/java/com/example/sosemergency/ui/contact/ContactModel.java
@@ -0,0 +1,27 @@
+package com.example.sosemergency.ui.contact;
+
+public class ContactModel {
+    String name;
+    String phone;
+
+    public ContactModel(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
