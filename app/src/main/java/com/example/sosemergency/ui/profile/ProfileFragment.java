520874ea306851c632a900e9eb9d2418b2b1fc81
chaimae <dardourichaimae0@gmail.com>
2023-11-26 00:58:46 +0100

    Initial UI for profile page

--- app/src/main/java/com/example/sosemergency/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/sosemergency/ui/profile/ProfileFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -17,18 +19,62 @@ import com.example.sosemergency.databinding.FragmentProfileBinding;
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+    private ProfileViewModel notificationsViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        notificationsViewModel = new ViewModelProvider(requireActivity()).get(ProfileViewModel.class);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.title_profile);
-        ProfileViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(ProfileViewModel.class);
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textProfile;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView profileFullName = binding.profileFullName;
+        final ImageView profileEditIcon = binding.profileEditIcon;
+        final ImageView profileSaveIcon = binding.profileSaveIcon;
+
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), profileFullName::setText);
+
+        profileEditIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Make the TextView editable
+                profileFullName.setFocusableInTouchMode(true);
+                profileFullName.setFocusable(true);
+                profileFullName.requestFocus();
+
+                // Show the save icon and hide the edit icon
+                profileEditIcon.setVisibility(View.GONE);
+                profileSaveIcon.setVisibility(View.VISIBLE);
+            }
+        });
+
+        profileSaveIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Save the modified text
+                String modifiedFullName = profileFullName.getText().toString();
+
+                // Update the ViewModel with the modified text
+                notificationsViewModel.setText(modifiedFullName);
+
+                // Perform your save logic here if needed
+
+                // Make the TextView non-editable again
+                profileFullName.setFocusable(false);
+                profileFullName.setFocusableInTouchMode(false);
+
+                // Show the edit icon and hide the save icon
+                profileEditIcon.setVisibility(View.VISIBLE);
+                profileSaveIcon.setVisibility(View.GONE);
+            }
+        });
+
         return root;
     }
 
