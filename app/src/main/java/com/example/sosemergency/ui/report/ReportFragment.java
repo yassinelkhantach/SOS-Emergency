9d2b854becd16c6546f5435fcdd9b3f0d78aa1fa
Yassine EL KHANTACH <yassine.elkhantach@hotmail.com>
2023-11-26 04:10:11 +0100

    Fixed Map visualization issue by adding API KEY of Map SDK for android, and making some actions like picking a position on the map, search a specific position. Also emergency type selection buttons color get changed after selecting one (RadioButton effect)

--- app/src/main/java/com/example/sosemergency/ui/report/ReportFragment.java
+++ app/src/main/java/com/example/sosemergency/ui/report/ReportFragment.java
@@ -1,109 +1,331 @@
 package com.example.sosemergency.ui.report;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.PorterDuff;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.sosemergency.R;
 import com.example.sosemergency.databinding.FragmentReportBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ReportFragment extends Fragment implements OnMapReadyCallback {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * The ReportFragment class represents the fragment responsible for reporting emergencies.
+ * It includes functionality for location reporting, map interaction, and emergency type selection.
+ */
+public class ReportFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener, View.OnClickListener {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private FragmentReportBinding binding;
     private RadioGroup radioGroup;
+    private GoogleMap googleMap;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Button reportLocationButton;
+    private EditText report_search_location;
+    private List<Marker> markers = new ArrayList<>();
+    private RadioButton reportFireButton;
+    private RadioButton reportAccidentButton;
+    private RadioButton reportEarthquakeButton;
+    private RadioButton reportCrimeButton;
+    private RadioButton reportFloodButton;
+    private RadioButton reportBriberyButton;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.title_report);
-        ReportViewModel reportViewModel =
-                new ViewModelProvider(this).get(ReportViewModel.class);
+        ((AppCompatActivity) requireActivity()).getSupportActionBar().setTitle(R.string.title_report);
+        ReportViewModel reportViewModel = new ViewModelProvider(this).get(ReportViewModel.class);
 
         binding = FragmentReportBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View view = binding.getRoot();
 
-        final TextView textView = binding.textReport;
-        reportViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Initialize FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext());
 
-        // Find the RadioGroup in your layout
-        radioGroup = root.findViewById(R.id.radioGroup);
-
-        // Set a listener for the radio group
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                Log.d("RadioButtons", "Checked ID: " + checkedId);
-                updateIconColors(root,checkedId);
-            }
-        });
-
-        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.report_map);
-        mapFragment.getMapAsync(this);
-
-        return root;
-    }
-
-    private void updateIconColors(View root,int checkedId) {
-        Log.d("RadioButtons", "Checked ID: " + checkedId);
-        RadioButton reportFireButton = root.findViewById(R.id.report_fire_button);
-        RadioButton reportAccidentButton = root.findViewById(R.id.report_accident_button);
-        RadioButton reportEarthquakeButton = root.findViewById(R.id.report_earthquake_button);
-        RadioButton reportCrimeButton = root.findViewById(R.id.report_crime_button);
-        RadioButton reportFloodButton = root.findViewById(R.id.report_flood_button);
-        RadioButton reportBriberyButton = root.findViewById(R.id.report_bribery_button);
-
-        // Reset color filter for all buttons
-        reportFireButton.getCompoundDrawables()[1].setColorFilter(null);
-        reportAccidentButton.getCompoundDrawables()[1].setColorFilter(null);
-        reportEarthquakeButton.getCompoundDrawables()[1].setColorFilter(null);
-        reportCrimeButton.getCompoundDrawables()[1].setColorFilter(null);
-        reportFloodButton.getCompoundDrawables()[1].setColorFilter(null);
-        reportBriberyButton.getCompoundDrawables()[1].setColorFilter(null);
-
-        // Set color filter for the checked button
-        if (checkedId == R.id.report_fire_button) {
-            int color = ContextCompat.getColor(requireContext(), R.color.danger);
-            reportFireButton.getCompoundDrawables()[1].setColorFilter(color, PorterDuff.Mode.SRC_IN);
-        } else if (checkedId == R.id.report_accident_button) {
-            int color = ContextCompat.getColor(requireContext(), R.color.danger);
-            reportAccidentButton.getCompoundDrawables()[1].setColorFilter(color, PorterDuff.Mode.SRC_IN);
-        } else if (checkedId == R.id.report_earthquake_button) {
-            int color = ContextCompat.getColor(requireContext(), R.color.danger);
-            reportEarthquakeButton.getCompoundDrawables()[1].setColorFilter(color, PorterDuff.Mode.SRC_IN);
-        } else if (checkedId == R.id.report_crime_button) {
-            int color = ContextCompat.getColor(requireContext(), R.color.danger);
-            reportCrimeButton.getCompoundDrawables()[1].setColorFilter(color, PorterDuff.Mode.SRC_IN);
-        } else if (checkedId == R.id.report_flood_button) {
-            int color = ContextCompat.getColor(requireContext(), R.color.danger);
-            reportFloodButton.getCompoundDrawables()[1].setColorFilter(color, PorterDuff.Mode.SRC_IN);
-        } else if (checkedId == R.id.report_bribery_button) {
-            int color = ContextCompat.getColor(requireContext(), R.color.danger);
-            reportBriberyButton.getCompoundDrawables()[1].setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.report_map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
         }
 
+        // Set up the map click listener
+        if (googleMap != null) {
+            googleMap.setOnMapClickListener(this);
+        }
+
+        // Set up the EditText and button for address lookup
+        report_search_location = view.findViewById(R.id.report_search_location);
+        reportLocationButton = view.findViewById(R.id.report_location_button);
+
+        // Add the click listener for the button
+        reportLocationButton.setOnClickListener(v -> lookupAddress());
+
+        // Find the RadioGroup in your layout
+        radioGroup = view.findViewById(R.id.radioGroup);
+
+        // Set click listener for each RadioButton
+        reportFireButton = view.findViewById(R.id.report_fire_button);
+        reportAccidentButton = view.findViewById(R.id.report_accident_button);
+        reportEarthquakeButton = view.findViewById(R.id.report_earthquake_button);
+        reportCrimeButton = view.findViewById(R.id.report_crime_button);
+        reportFloodButton = view.findViewById(R.id.report_flood_button);
+        reportBriberyButton = view.findViewById(R.id.report_bribery_button);
+
+        reportFireButton.setOnClickListener(this);
+        reportAccidentButton.setOnClickListener(this);
+        reportEarthquakeButton.setOnClickListener(this);
+        reportCrimeButton.setOnClickListener(this);
+        reportFloodButton.setOnClickListener(this);
+        reportBriberyButton.setOnClickListener(this);
+
+        return view;
     }
+
+    @Override
+    public void onClick(View v) {
+        int checkedId = -1;
+
+        // Identify which RadioButton was clicked
+        if (v.getId() == R.id.report_fire_button) {
+            checkedId = R.id.report_fire_button;
+        } else if (v.getId() == R.id.report_accident_button) {
+            checkedId = R.id.report_accident_button;
+        } else if (v.getId() == R.id.report_earthquake_button) {
+            checkedId = R.id.report_earthquake_button;
+        } else if (v.getId() == R.id.report_crime_button) {
+            checkedId = R.id.report_crime_button;
+        } else if (v.getId() == R.id.report_flood_button) {
+            checkedId = R.id.report_flood_button;
+        } else if (v.getId() == R.id.report_bribery_button) {
+            checkedId = R.id.report_bribery_button;
+        }
+
+        // Calling updateIconColors method to update colors and perform other actions
+        updateIconColors(v, checkedId);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        // Customize your map here
+        this.googleMap = googleMap;
+
+        // Enable the My Location layer and the related control on the map
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            googleMap.setMyLocationEnabled(true);
+        } else {
+            // Request location permission from the user
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+        // Load the current location on the map
+        checkLocationPermissionAndLoadLocation();
+
+        // Set up the map click listener
+        googleMap.setOnMapClickListener(this);
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        // Clear existing markers
+        clearMarkers();
+
+        // Add a marker at the clicked position with latitude and longitude as the title
+        String markerTitle = latLng.latitude + "," + latLng.longitude;
+        Marker clickedMarker = googleMap.addMarker(new MarkerOptions().position(latLng).title(markerTitle));
+        markers.add(clickedMarker);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, enable My Location layer
+                if (googleMap != null) {
+                    if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    googleMap.setMyLocationEnabled(true);
+                }
+            } else {
+                // Permission denied, handle accordingly
+                Toast.makeText(requireContext(), "Location permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
 
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
+    private void centerMapOnMyLocation() {
+        if (googleMap != null) {
+            if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                // Enable My Location layer if permission granted
+                googleMap.setMyLocationEnabled(true);
 
+                // Get the last known location
+                FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext());
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(requireActivity(), location -> {
+                            if (location != null) {
+                                LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f));
+                            }
+                        });
+            } else {
+                //TODO: We should handle the case where location permission is not granted
+            }
+        }
+    }
+
+    private void updateIconColors(View view, int checkedId) {
+        // Reset color filter for all buttons
+        resetColorFilter(reportFireButton);
+        resetColorFilter(reportAccidentButton);
+        resetColorFilter(reportEarthquakeButton);
+        resetColorFilter(reportCrimeButton);
+        resetColorFilter(reportFloodButton);
+        resetColorFilter(reportBriberyButton);
+
+        // Set color filter for the checked button
+        RadioButton checkedButton = view.findViewById(checkedId);
+        if (checkedButton != null) {
+            int color = ContextCompat.getColor(requireContext(), R.color.danger);
+            checkedButton.getCompoundDrawables()[1].setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        }
+    }
+
+    // Helper method to reset color filter
+    private void resetColorFilter(RadioButton radioButton) {
+        if (radioButton != null) {
+            radioButton.getCompoundDrawables()[1].setColorFilter(null);
+        }
+    }
+
+    private void checkLocationPermissionAndLoadLocation() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            // Permission granted, we load current location
+            loadMyLocation();
+        } else {
+            // Permission not granted, we request it
+            ActivityCompat.requestPermissions(requireActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void loadMyLocation() {
+        if (googleMap != null) {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(requireActivity(), this::onLocationSuccess);
+        }
+    }
+
+    private void onLocationSuccess(Location location) {
+        if (location != null) {
+            LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+            // Clear existing markers
+            clearMarkers();
+
+            // Add a marker for the current location
+            Marker myLocationMarker = googleMap.addMarker(new MarkerOptions().position(currentLatLng).title("My Location"));
+            markers.add(myLocationMarker);
+
+            // Move the camera to the current location
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(12.0f));
+        }
+    }
+
+    private void lookupAddress() {
+        String address = report_search_location.getText().toString().trim();
+        if (!address.isEmpty()) {
+            Geocoder geocoder = new Geocoder(requireContext());
+            try {
+                List<Address> addresses = geocoder.getFromLocationName(address, 1);
+                if (!addresses.isEmpty()) {
+                    Address foundAddress = addresses.get(0);
+                    LatLng location = new LatLng(foundAddress.getLatitude(), foundAddress.getLongitude());
+
+                    // Clear existing markers
+                    clearMarkers();
+
+                    // Add a marker at the found location
+                    String markerTitle = "Address: " + foundAddress.getAddressLine(0);
+                    Marker addressMarker = googleMap.addMarker(new MarkerOptions().position(location).title(markerTitle));
+                    markers.add(addressMarker);
+
+                    // Move the camera to the found location
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+                    googleMap.animateCamera(CameraUpdateFactory.zoomTo(12.0f));
+                } else {
+                    Toast.makeText(requireContext(), "Address not found", Toast.LENGTH_SHORT).show();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(requireContext(), "Please enter an address", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void clearMarkers() {
+        if (markers != null) {
+            for (Marker marker : markers) {
+                marker.remove();
+            }
+            markers.clear();
+        }
     }
 }
