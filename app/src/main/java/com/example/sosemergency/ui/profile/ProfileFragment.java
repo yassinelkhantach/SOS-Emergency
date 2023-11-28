4a95ff141f8388ff4cb1636fac624f47efa637a7
chaimae <dardourichaimae0@gmail.com>
2023-11-28 19:53:25 +0100

    Migration of profile components to CardView, Fixed spacing issues

--- app/src/main/java/com/example/sosemergency/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/sosemergency/ui/profile/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,71 +17,68 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.sosemergency.R;
 import com.example.sosemergency.databinding.FragmentProfileBinding;
 
-public class ProfileFragment extends Fragment {
+    public class ProfileFragment extends Fragment {
 
-    private FragmentProfileBinding binding;
-    private ProfileViewModel notificationsViewModel;
+        private FragmentProfileBinding binding;
+        private ProfileViewModel notificationsViewModel;
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        notificationsViewModel = new ViewModelProvider(requireActivity()).get(ProfileViewModel.class);
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            notificationsViewModel = new ViewModelProvider(requireActivity()).get(ProfileViewModel.class);
+        }
+
+        public View onCreateView(@NonNull LayoutInflater inflater,
+                                 ViewGroup container, Bundle savedInstanceState) {
+            ((AppCompatActivity) requireActivity()).getSupportActionBar().setTitle(R.string.title_profile);
+
+            binding = FragmentProfileBinding.inflate(inflater, container, false);
+            View root = binding.getRoot();
+
+            final EditText profileFullName = binding.profileFullName;
+            final ImageView profileEditIcon = binding.profileEditIcon;
+            final ImageView profileSaveIcon = binding.profileSaveIcon;
+
+            // Use lambda expression for click listeners
+            profileEditIcon.setOnClickListener(v -> enableEditing(profileFullName, profileEditIcon, profileSaveIcon));
+            profileSaveIcon.setOnClickListener(v -> saveChanges(profileFullName, profileEditIcon, profileSaveIcon));
+
+            // Observe the LiveData directly
+            notificationsViewModel.getText().observe(getViewLifecycleOwner(), profileFullName::setText);
+
+            return root;
+        }
+
+        private void enableEditing(EditText editText, ImageView editIcon, ImageView saveIcon) {
+            // Make the TextView editable
+            editText.setFocusableInTouchMode(true);
+            editText.setFocusable(true);
+            editText.requestFocus();
+
+            // Show the save icon and hide the edit icon
+            editIcon.setVisibility(View.GONE);
+            saveIcon.setVisibility(View.VISIBLE);
+        }
+
+        private void saveChanges(EditText editText, ImageView editIcon, ImageView saveIcon) {
+            // Save the modified text
+            String modifiedFullName = editText.getText().toString();
+
+            // Update the ViewModel with the modified text
+            notificationsViewModel.setText(modifiedFullName);
+
+            // Make the TextView non-editable again
+            editText.setFocusable(false);
+            editText.setFocusableInTouchMode(false);
+
+            // Show the edit icon and hide the save icon
+            editIcon.setVisibility(View.VISIBLE);
+            saveIcon.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onDestroyView() {
+            super.onDestroyView();
+            binding = null;
+        }
     }
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.title_profile);
-
-        binding = FragmentProfileBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView profileFullName = binding.profileFullName;
-        final ImageView profileEditIcon = binding.profileEditIcon;
-        final ImageView profileSaveIcon = binding.profileSaveIcon;
-
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), profileFullName::setText);
-
-        profileEditIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Make the TextView editable
-                profileFullName.setFocusableInTouchMode(true);
-                profileFullName.setFocusable(true);
-                profileFullName.requestFocus();
-
-                // Show the save icon and hide the edit icon
-                profileEditIcon.setVisibility(View.GONE);
-                profileSaveIcon.setVisibility(View.VISIBLE);
-            }
-        });
-
-        profileSaveIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Save the modified text
-                String modifiedFullName = profileFullName.getText().toString();
-
-                // Update the ViewModel with the modified text
-                notificationsViewModel.setText(modifiedFullName);
-
-                // Perform your save logic here if needed
-
-                // Make the TextView non-editable again
-                profileFullName.setFocusable(false);
-                profileFullName.setFocusableInTouchMode(false);
-
-                // Show the edit icon and hide the save icon
-                profileEditIcon.setVisibility(View.VISIBLE);
-                profileSaveIcon.setVisibility(View.GONE);
-            }
-        });
-
-        return root;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-}
