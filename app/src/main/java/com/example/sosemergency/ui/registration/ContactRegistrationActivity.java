1feac7aa2ef1a38556e1ef2689ba8961925c4519
chaimae <dardourichaimae0@gmail.com>
2023-12-07 17:32:17 +0100

    pick contact phone number to a textView

--- app/src/main/java/com/example/sosemergency/ui/registration/ContactRegistrationActivity.java
+++ app/src/main/java/com/example/sosemergency/ui/registration/ContactRegistrationActivity.java
@@ -1,17 +1,77 @@
 package com.example.sosemergency.ui.registration;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sosemergency.R;
 
 public class ContactRegistrationActivity extends AppCompatActivity {
+    private static final int RESULT_PICK_CONTACT = 1;
+
+    ImageView addIcon;
+    TextView textChoose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_contact_registration);
 
+        addIcon = findViewById(R.id.addIcon);
+        textChoose = findViewById(R.id.textChoose);
+
+        addIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+                startActivityForResult(in, RESULT_PICK_CONTACT);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case RESULT_PICK_CONTACT:
+                    contactPicked(data);
+                    break;
+            }
+        } else {
+            Toast.makeText(this, "Failed To pick contact", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void contactPicked(Intent data) {
+        Cursor cursor = null;
+
+        try {
+            String phoneNo = null;
+            Uri uri = data.getData();
+            cursor = getContentResolver().query(uri, null, null, null, null);
+
+            if (cursor != null && cursor.moveToFirst()) {
+                int phoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                phoneNo = cursor.getString(phoneIndex);
+                textChoose.setText(phoneNo);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/sosemergency/ui/registration/UserRegistrationActivity.java
+++ app/src/main/java/com/example/sosemergency/ui/registration/UserRegistrationActivity.java
@@ -1,31 +1,54 @@
 package com.example.sosemergency.ui.registration;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.sosemergency.R;
 
 public class UserRegistrationActivity extends AppCompatActivity {
+    EditText fullName;
+    EditText dateOfBirth;
+    EditText Country;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
+        fullName = findViewById(R.id.registrationFullName);
+        dateOfBirth = findViewById(R.id.registrationDateOfBirth);
+        Country = findViewById(R.id.registrationCountry);
+        ImageView dotIcon = findViewById(R.id.registrationDot);
 
-        getSupportActionBar().setTitle(R.string.app_name);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-    @Override
-    public boolean onSupportNavigateUp() {
-        // Navigate back to the home page and then to main activity.
-        getOnBackPressedDispatcher().onBackPressed();
-        return true;
+        // Set OnClickListener for the dot icon
+        dotIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Check if all required fields are filled
+                if (isFieldsFilled()) {
+                    // All fields are filled, proceed to Activity 2
+                    startActivity(new Intent(UserRegistrationActivity.this, ContactRegistrationActivity.class));
+                } else {
+                    // Display a message or perform some action if fields are not filled
+                    Toast.makeText(UserRegistrationActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.top_nav_menu,menu);
-        return true;
+    // Helper method to check if all required fields are filled
+    private boolean isFieldsFilled() {
+        String Name = fullName.getText().toString();
+        String dob = dateOfBirth.getText().toString();
+        String country = Country.getText().toString();
+
+        // Check if any of the fields is empty
+        return !TextUtils.isEmpty(Name) && !TextUtils.isEmpty(dob) && !TextUtils.isEmpty(country);
     }
 }
-
