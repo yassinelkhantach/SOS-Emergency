d55faf9513d922b86b125315b8eeb8978777121a
chaimae <dardourichaimae0@gmail.com>
2023-12-09 11:41:02 +0100

    Add doc-style code for java classes

--- app/src/main/java/com/example/sosemergency/ui/registration/ContactRegistrationActivity.java
+++ app/src/main/java/com/example/sosemergency/ui/registration/ContactRegistrationActivity.java
@@ -22,44 +22,58 @@ import com.example.sosemergency.ui.contact.ContactModel;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * ContactRegistrationActivity - Activity for registering contacts.
+ *
+ * This activity allows the user to pick contacts from the device's contact list and
+ * displays the selected contacts in a RecyclerView. The user can add contacts until
+ * a maximum of four contacts is reached. The selected contacts can be removed, and the
+ * user can navigate to other activities using icons provided on the screen.
+ */
 public class ContactRegistrationActivity extends AppCompatActivity {
+
+    // Constant to identify the result of picking a contact
     private static final int RESULT_PICK_CONTACT = 1;
 
+    // UI elements
     ImageView addIcon;
     ImageView passIcon;
     TextView textChoose;
     RecyclerView contactRecyclerView;
     ContactAdapter contactAdapter;
-
-    List<ContactModel> contactList = new ArrayList<>();
     ImageView dotIcon;
+
+    // List to store selected contacts
+    List<ContactModel> contactList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_registration);
 
+        // Initialize UI elements
         addIcon = findViewById(R.id.addIcon);
         textChoose = findViewById(R.id.textChoose);
         contactRecyclerView = findViewById(R.id.contactRecyclerView);
-        ImageView dotIcon = findViewById(R.id.registrationDot);
-
+        dotIcon = findViewById(R.id.registrationDot);
         passIcon = findViewById(R.id.passIcon);
 
-        // Initialize your RecyclerView adapter (assuming you have a custom adapter)
+        // Initialize RecyclerView and its adapter
         contactAdapter = new ContactAdapter(contactList, passIcon, addIcon, dotIcon);
         contactRecyclerView.setAdapter(contactAdapter);
         contactRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        // Set click listeners for icons
         addIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Check if the user has already picked two contacts
+                // Check if the user has already picked four contacts
                 if (contactList.size() < 4) {
+                    // Start the contact picking intent
                     Intent in = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
                     startActivityForResult(in, RESULT_PICK_CONTACT);
                 } else {
-                    // Display a toast message indicating that the user can only pick two contacts
+                    // Display a toast message indicating the maximum contacts reached
                     Toast.makeText(ContactRegistrationActivity.this, "You have already picked four contacts", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -67,17 +81,20 @@ public class ContactRegistrationActivity extends AppCompatActivity {
         passIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    startActivity(new Intent(ContactRegistrationActivity.this, BootstrapActivity.class));
+                // Start BootstrapActivity when passIcon is clicked
+                startActivity(new Intent(ContactRegistrationActivity.this, BootstrapActivity.class));
             }
         });
         dotIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Start UserRegistrationActivity when dotIcon is clicked
                 startActivity(new Intent(ContactRegistrationActivity.this, UserRegistrationActivity.class));
             }
         });
     }
 
+    // Handle the result of picking a contact
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -85,18 +102,22 @@ public class ContactRegistrationActivity extends AppCompatActivity {
         if (resultCode == RESULT_OK) {
             switch (requestCode) {
                 case RESULT_PICK_CONTACT:
+                    // Process the picked contact
                     contactPicked(data);
                     break;
             }
         } else {
+            // Display a toast message for a failed contact pick
             Toast.makeText(this, "Failed To pick contact", Toast.LENGTH_SHORT).show();
         }
     }
 
+    // Process the picked contact and add it to the list
     private void contactPicked(Intent data) {
         Cursor cursor = null;
 
         try {
+            // Extract contact information from the cursor
             String contactName = null;
             String contactPhone = null;
             Uri uri = data.getData();
@@ -109,7 +130,7 @@ public class ContactRegistrationActivity extends AppCompatActivity {
                 contactName = cursor.getString(nameIndex);
                 contactPhone = cursor.getString(phoneIndex);
 
-                // Check if the contact already exists in the list
+                // Check if the contact is not already picked
                 if (!isContactAlreadyPicked(contactName, contactPhone)) {
                     // Create a new ContactModel
                     ContactModel contact = new ContactModel(contactName, contactPhone);
@@ -124,21 +145,29 @@ public class ContactRegistrationActivity extends AppCompatActivity {
                     Toast.makeText(this, "This contact is already picked", Toast.LENGTH_SHORT).show();
                 }
             }
-            if (contactList.size() >= 1 && contactList.size() < 4) {
-                // If one or more than one contact is chosen but less than 4, show both the "Add" icon and the "Select" icon
-                passIcon.setVisibility(View.VISIBLE);
-                dotIcon.setVisibility(View.GONE);
-            } else if (contactList.size() == 4) {
-                addIcon.setVisibility(View.GONE);
-                passIcon.setVisibility(View.VISIBLE);
-                addIcon.setVisibility(View.GONE);
-            }
+            // Update visibility of icons based on the number of selected contacts
+            updateIconVisibility();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             if (cursor != null && !cursor.isClosed()) {
+                // Close the cursor if it's not null
                 cursor.close();
             }
+        }
+    }
+
+    // Update visibility of icons based on the number of selected contacts
+    private void updateIconVisibility() {
+        if (contactList.size() >= 1 && contactList.size() < 4) {
+            // If one or more than one contact is chosen but less than 4, show both the "Add" icon and the "Select" icon
+            passIcon.setVisibility(View.VISIBLE);
+            dotIcon.setVisibility(View.GONE);
+        } else if (contactList.size() == 4) {
+            // If 4 contacts are chosen, hide the "Add" icon and show only the "Select" icon
+            addIcon.setVisibility(View.GONE);
+            passIcon.setVisibility(View.VISIBLE);
+            dotIcon.setVisibility(View.GONE);
         }
     }
 
--- app/src/main/java/com/example/sosemergency/ui/registration/UserRegistrationActivity.java
+++ app/src/main/java/com/example/sosemergency/ui/registration/UserRegistrationActivity.java
@@ -15,22 +15,31 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.sosemergency.R;
 
+/**
+ * UserRegistrationActivity - Activity for user registration.
+ *
+ * This activity allows the user to enter their full name, date of birth, and their country.
+ * The user can proceed to the next activity by clicking the dot icon after
+ * filling in all required fields.
+ */
 public class UserRegistrationActivity extends AppCompatActivity {
+    // UI elements
     EditText fullName;
     EditText dateOfBirth;
     AutoCompleteTextView country;
-
     ImageView dotIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
+
+        // Initialize UI elements
         fullName = findViewById(R.id.registrationFullName);
         dateOfBirth = findViewById(R.id.registrationDateOfBirth);
         country = findViewById(R.id.registrationCountry);
         dotIcon = findViewById(R.id.registrationDot);
 
-        // Set up an array of country names
+        // Set up an array of country names for the autocomplete feature
         String[] countries = {"Morocco", "France", "England"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, countries);
         country.setAdapter(adapter);
@@ -44,18 +53,24 @@ public class UserRegistrationActivity extends AppCompatActivity {
                     // All fields are filled, proceed to Activity 2
                     startActivity(new Intent(UserRegistrationActivity.this, ContactRegistrationActivity.class));
                 } else {
-                    // Display a message or perform some action if fields are not filled
+                    // Display a message for perform some action if fields are not filled
                     Toast.makeText(UserRegistrationActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    // Helper method to check if all required fields are filled
+    /**
+     * Helper method to check if all required fields are filled.
+     *
+     * @return True if all fields are filled, false otherwise.
+     */
     private boolean isFieldsFilled() {
+        // Extract text from UI elements
         String name = fullName.getText().toString();
         String dob = dateOfBirth.getText().toString();
         String selectedCountry = country.getText().toString();
+
         // Check if any of the fields is empty
         return !TextUtils.isEmpty(name) && !TextUtils.isEmpty(dob) && !TextUtils.isEmpty(selectedCountry);
     }
