071c5de8b4c5e632edc9f7ad49411f3e71a84849
chaimae <dardourichaimae0@gmail.com>
2023-12-09 12:24:28 +0100

    Add doc-style code for profile fragement

--- app/src/main/java/com/example/sosemergency/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/sosemergency/ui/profile/ProfileFragment.java
@@ -40,14 +40,30 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * ProfileFragment - Fragment for displaying and editing user profile information.
+ *
+ * This fragment allows the user to view and edit their profile details, including full name,
+ * date of birth, age, blood type, height, weight, and allergies. The user can toggle between
+ * edit mode and view mode, and can save changes made in edit mode.
+ */
 public class ProfileFragment extends Fragment {
 
+    // Data binding
     private FragmentProfileBinding binding;
+
+    // ViewModel for profile information
     private ProfileViewModel notificationsViewModel;
+
+    // Date picker listener
     private DatePickerDialog.OnDateSetListener mDateSetListener;
+
+    // Dialogs for blood type, height, and weight selection
     private Dialog bloodTypeDialog;
     private Dialog heightDialog;
     private Dialog weightDialog;
+
+    // UI elements for allergies
     private CardView appleAllergie;
     private CardView grapeAllergie;
     private CardView strawberryAllergie;
@@ -56,23 +72,30 @@ public class ProfileFragment extends Fragment {
     private CardView fishAllergie;
     private CardView otherAllergie;
 
+    // Edit and save icons for allergies
     private ImageView allergiesEditIcon;
     private ImageView allergiesSaveIcon;
+
+    // Edit mode flag
     private boolean isEditMode = false;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Initialize the ViewModel
         notificationsViewModel = new ViewModelProvider(requireActivity()).get(ProfileViewModel.class);
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        // Set the ActionBar title
         ((AppCompatActivity) requireActivity()).getSupportActionBar().setTitle(R.string.title_profile);
 
+        // Inflate the layout using data binding
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // Initialize UI elements
         final EditText profileFullName = binding.profileFullName;
         final EditText profileBD = binding.profileBD;
         final ImageView profileEditIcon = binding.profileEditIcon;
@@ -201,31 +224,47 @@ public class ProfileFragment extends Fragment {
         saveIcon.setVisibility(View.GONE);
     }
 
-    private void showDateDialog(EditText profileDate) {
-
-    }
-
+    /**
+     * Show a date picker dialog for selecting a date.
+     *
+     * @param profileDate The EditText where the selected date will be displayed.
+     */
     private void showDatePicker(EditText profileDate) {
+        // Get the current date
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
         int day = cal.get(Calendar.DAY_OF_MONTH);
 
+        // Create a DatePickerDialog with a custom theme and set the date listener
         DatePickerDialog dialog = new DatePickerDialog(getActivity(),R.style.DatePickerTheme, mDateSetListener,year,month,day);
+
+        // Set the background of the dialog to white
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+
+        // Show the date picker dialog
         dialog.show();
     }
 
-    // Helper method to calculate age
+    /**
+     * Calculate the age based on the date of birth and the current date.
+     *
+     * @param dateOfBirth The date of birth.
+     * @param dateOfNow   The current date.
+     * @return The calculated age.
+     */
     private int calculateAge(Date dateOfBirth, Date dateOfNow) {
+        // Create Calendar instances for the date of birth and the current date
         Calendar dob = Calendar.getInstance();
         dob.setTime(dateOfBirth);
 
         Calendar now = Calendar.getInstance();
         now.setTime(dateOfNow);
 
+        // Calculate the age based on the difference in years
         int age = now.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
 
+        // Adjust the age if the birthday hasn't occurred yet in the current year
         if (now.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)) {
             age--;
         }
@@ -233,13 +272,19 @@ public class ProfileFragment extends Fragment {
         return age;
     }
 
+    /**
+     * Set up and initialize the blood type selection dialog.
+     * The dialog allows the user to choose their blood type from a spinner.
+     */
     private void setupBloodTypeDialog() {
+        // Create a new Dialog instance for blood type selection
         bloodTypeDialog = new Dialog(requireContext());
 
         // Inflate the layout for the dialog
         View dialogView = getLayoutInflater().inflate(R.layout.blood_type_dialog, null);
         bloodTypeDialog.setContentView(dialogView);
 
+        // Find views in the dialog layout
         Spinner spinnerBloodType = bloodTypeDialog.findViewById(R.id.spinnerBloodType);
         Button btnSelectBloodType = bloodTypeDialog.findViewById(R.id.btnSelectBloodType);
 
@@ -264,10 +309,14 @@ public class ProfileFragment extends Fragment {
 
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
-                // Do nothing here
             }
         });
 
+        /**
+         * OnClickListener for the button to confirm the selected blood type in the blood type dialog.
+         * This method updates the ViewModel with the selected blood type, updates the associated EditText,
+         * and dismisses the blood type selection dialog.
+         */
         btnSelectBloodType.setOnClickListener(v -> {
             // Update the ViewModel with the selected blood type
             String selectedBloodType = (String) spinnerBloodType.getSelectedItem();
@@ -276,74 +325,114 @@ public class ProfileFragment extends Fragment {
             // Update the EditText in your CardView with the selected blood type
             EditText editText = requireView().findViewById(R.id.profileBlood);
             editText.setText(selectedBloodType);
+
+            // Dismiss the blood type selection dialog
             bloodTypeDialog.dismiss();
         });
     }
+    /**
+     * Show the blood type selection dialog.
+     * This method checks if the bloodTypeDialog is not null and then shows the dialog.
+     */
     private void showBloodTypeDialog() {
+        // Check if the blood type dialog is not null
         if (bloodTypeDialog != null) {
+            // Show the blood type selection dialog
             bloodTypeDialog.show();
         }
     }
 
+    /**
+     * Set up and initialize the height selection dialog.
+     * The dialog allows the user to enter their height and save the selection.
+     */
     private void setupHeightDialog() {
+        // Create a new Dialog instance for height selection
         heightDialog = new Dialog(requireContext());
 
         // Inflate the layout for the dialog
         View dialogView = getLayoutInflater().inflate(R.layout.height_dialog, null);
         heightDialog.setContentView(dialogView);
 
+        // Find views in the dialog layout
         EditText editTextHeight = heightDialog.findViewById(R.id.editTextHeight);
         Button btnSelectHeight = heightDialog.findViewById(R.id.btnSelectHeight);
 
+        // Set up a listener for the button to confirm the entered height
         btnSelectHeight.setOnClickListener(v -> {
             // Update the ViewModel with the entered height
             String enteredHeight = editTextHeight.getText().toString();
-            // You can use the entered height as needed
 
             // Update the EditText in your CardView with the entered height
             EditText editText = requireView().findViewById(R.id.profileHeight);
             editText.setText(enteredHeight);
 
+            // Dismiss the height selection dialog
             heightDialog.dismiss();
         });
     }
 
+    /**
+     * Show the height selection dialog.
+     * This method checks if the heightDialog is not null and then shows the dialog.
+     */
     private void showHeightDialog() {
+        // Check if the height selection dialog is not null
         if (heightDialog != null) {
+            // Show the height selection dialog
             heightDialog.show();
         }
     }
 
+    /**
+     * Set up and initialize the weight selection dialog.
+     * The dialog allows the user to enter their weight and save the selection.
+     */
     private void setupWeightDialog() {
+        // Create a new Dialog instance for weight selection
         weightDialog = new Dialog(requireContext());
 
         // Inflate the layout for the dialog
         View dialogView = getLayoutInflater().inflate(R.layout.weight_dialog, null);
         weightDialog.setContentView(dialogView);
 
+        // Find views in the dialog layout
         EditText editTextWeight = weightDialog.findViewById(R.id.editTextWeight);
         Button btnSelectWeight = weightDialog.findViewById(R.id.btnSelectWeight);
 
+        // Set up a listener for the button to confirm the entered weight
         btnSelectWeight.setOnClickListener(v -> {
             // Update the ViewModel with the entered weight
             String enteredWeight = editTextWeight.getText().toString();
-            // You can use the entered weight as needed
 
             // Update the EditText in your CardView with the entered weight
             EditText editText = requireView().findViewById(R.id.profileWeight);
             editText.setText(enteredWeight);
 
+            // Dismiss the weight selection dialog
             weightDialog.dismiss();
         });
     }
 
+    /**
+     * Show the weight selection dialog.
+     * This method checks if the weightDialog is not null and then shows the dialog.
+     */
     private void showWeightDialog() {
+        // Check if the weight selection dialog is not null
         if (weightDialog != null) {
+            // Show the weight selection dialog
             weightDialog.show();
         }
     }
 
+    /**
+     * Toggle the allergies edit mode.
+     * This method toggles the edit mode, showing or hiding the save and edit icons,
+     * and enabling or disabling click listeners based on the edit mode.
+     */
     private void toggleAllergiesEditMode() {
+        // Toggle the edit mode
         isEditMode = !isEditMode;
 
         // Show or hide the save icon based on the edit mode
@@ -356,7 +445,13 @@ public class ProfileFragment extends Fragment {
         setAllergiesClickability(isEditMode);
     }
 
+    /**
+     * Set the clickability of allergy cards.
+     *
+     * @param isClickable A boolean indicating whether the cards should be clickable or not.
+     */
     private void setAllergiesClickability(boolean isClickable) {
+        // Set the clickability of each allergy card based on the provided boolean
         appleAllergie.setClickable(isClickable);
         grapeAllergie.setClickable(isClickable);
         strawberryAllergie.setClickable(isClickable);
@@ -366,6 +461,10 @@ public class ProfileFragment extends Fragment {
         otherAllergie.setClickable(isClickable);
     }
 
+    /**
+     * Set up click listeners for each allergy card.
+     * This method assigns click listeners to each allergy card to toggle their selection.
+     */
     private void setupAllergyClickListeners() {
         // Set click listener for the Apple Allergy card
         appleAllergie.setOnClickListener(v -> toggleAllergySelection(appleAllergie));
@@ -389,19 +488,32 @@ public class ProfileFragment extends Fragment {
         otherAllergie.setOnClickListener(v -> toggleAllergySelection(otherAllergie));
     }
 
+    /**
+     * Toggle the selection state of an allergy card when in edit mode.
+     *
+     * @param allergyCard The CardView representing an allergy.
+     */
     private void toggleAllergySelection(CardView allergyCard) {
+        // Check if the activity is in edit mode
         if (isEditMode) {
             // Toggle the background color between white and the default color
             int defaultColor = ContextCompat.getColor(requireContext(), R.color.extra_white);
             int selectedColor = ContextCompat.getColor(requireContext(), R.color.white);
 
+            // Check the current color of the allergy card background
             if (allergyCard.getCardBackgroundColor().getDefaultColor() == defaultColor) {
+                // Set the background color to the selected color if not already selected
                 allergyCard.setCardBackgroundColor(selectedColor);
             } else {
+                // Set the background color to the default color if already selected
                 allergyCard.setCardBackgroundColor(defaultColor);
             }
         }
     }
+
+    /**
+     * Save the selected allergies and update UI accordingly.
+     */
     private void saveSelectedAllergies() {
 
         // After saving, hide the save icon and show the edit icon
@@ -412,9 +524,10 @@ public class ProfileFragment extends Fragment {
         setAllergiesClickability(false);
     }
 
-
-
-
+    /**
+     * Lifecycle method called when the view is about to be destroyed.
+     * It is responsible for cleaning up references to the view binding.
+     */
     @Override
     public void onDestroyView() {
         super.onDestroyView();
--- app/src/main/java/com/example/sosemergency/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/sosemergency/ui/profile/ProfileViewModel.java
@@ -4,31 +4,57 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+/**
+ * ViewModel class responsible for handling data related to the user profile.
+ */
 public class ProfileViewModel extends ViewModel {
 
+    // MutableLiveData to store the user's full name
     private final MutableLiveData<String> mText;
+
+    // MutableLiveData to store the user's blood type
     private final MutableLiveData<String> bloodType; // Added for blood type
 
-
+    /**
+     * Constructor to initialize MutableLiveData instances.
+     */
     public ProfileViewModel() {
         mText = new MutableLiveData<>();
         bloodType = new MutableLiveData<>();
     }
 
+    /**
+     * Getter method for observing the user's full name.
+     *
+     * @return LiveData containing the user's full name.
+     */
     public LiveData<String> getText() {
         return mText;
     }
 
+    /**
+     * Setter method to update the user's full name.
+     *
+     * @param newText The new full name to set.
+     */
     public void setText(String newText) {
         mText.setValue(newText);
     }
 
-    // Getter method for blood type
+    /**
+     * Getter method for observing the user's blood type.
+     *
+     * @return LiveData containing the user's blood type.
+     */
     public LiveData<String> getBloodType() {
         return bloodType;
     }
 
-    // Setter method for blood type
+    /**
+     * Setter method to update the user's blood type.
+     *
+     * @param newBloodType The new blood type to set.
+     */
     public void setBloodType(String newBloodType) {
         bloodType.setValue(newBloodType);
     }
