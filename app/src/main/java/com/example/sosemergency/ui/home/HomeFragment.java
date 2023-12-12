b12da81edc4e08fee7c4d8e49c729c1349f35719
Yassine EL KHANTACH <yassine.elkhantach@hotmail.com>
2023-12-12 12:46:13 +0100

    removed some useless imported packages

--- app/src/main/java/com/example/sosemergency/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/sosemergency/ui/home/HomeFragment.java
@@ -2,30 +2,23 @@ package com.example.sosemergency.ui.home;
 
 import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ReportFragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-import com.example.sosemergency.MainActivity;
 import com.example.sosemergency.R;
 import com.example.sosemergency.databinding.FragmentHomeBinding;
 
